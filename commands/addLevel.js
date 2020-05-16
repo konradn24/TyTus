8481a6c84b6ca4b824889a15c88b1dafda81233c
konradn24 <54041190+konradn24@users.noreply.github.com>
2020-05-16 13:29:02 +0200

    Update addLevel.js

--- commands/addLevel.js
+++ commands/addLevel.js
@@ -6,7 +6,6 @@ module.exports.run = async (bot, message, args, database) => {
 
     if(!args[0]) return message.channel.send(":x: Określ któremu użytkownikowi chcesz zwiększyć poziom. Jeżeli potrzebujesz pomocy, wprowadź **/help addLevel**.");
     else if(!args[1]) return message.channel.send(":x: Określ ile poziomów chcesz dodać temu użytkownikowi.");
-    else if(typeof args[1] != "number") return message.channel.send(":x: Proszę podać liczbę większą od 0!");
     else if(args[1] < 1) return message.channel.send(":x: Proszę podać liczbę większą od 0! Jeżeli chcesz zmniejszyć poziom użytkownika wprowadź **/reduceLevel <@osoba> <ilosc>**.");
     
     let member = message.mentions.members.first();

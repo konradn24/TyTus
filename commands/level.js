562f78a6e3992e6f7eb4dd508e29ce8f9056b991
konradn24 <54041190+konradn24@users.noreply.github.com>
2020-06-15 14:20:18 +0200

    Update level.js

--- commands/level.js
+++ commands/level.js
@@ -11,13 +11,17 @@ module.exports.run = async (bot, message, args, database) => {
 
     response(message, loading);
 
-    database.query(`SELECT * FROM config WHERE id = 3`, (errC, rowsC) => {
+    database.query(`SELECT * FROM servers WHERE discordID = "${message.guild.id}"`, (errC, rowsC) => {
         if(errC) {
             console.log(errC);
-            return response(message, ":x: Wystąpił błąd podczas łączenia się z bazą danych (config). Spróbuj ponownie później.");
+            return response(message, ":x: Wystąpił błąd podczas łączenia się z bazą danych (servers - config). Spróbuj ponownie później.");
         }
 
-        if(rowsC[0].value === "false") return response(message, ":x: Nie możesz zobaczyć swojego poziomu, ponieważ poziomy za aktywność są wyłączone.");
+        if(rowsC.length < 1) return response(message, ":x: Nie możesz zobaczyć swojego poziomu, ponieważ poziomy za aktywność na tym serwerze są wyłączone.");
+        
+        let config = decode1(rowsC[0].config);
+
+        if(config[2] === "false") return response(message, ":x: Nie możesz zobaczyć swojego poziomu, ponieważ poziomy za aktywność na tym serwerze są wyłączone.");
 
         database.query(`SELECT * FROM members WHERE discordID = ${member.user.id}`, (err, rows) => {
             if(err) {
@@ -25,21 +29,54 @@ module.exports.run = async (bot, message, args, database) => {
                 return response(message, ":x: Wystąpił błąd podczas łączenia się z bazą danych (1). Spróbuj ponownie później.");
             }
 
-            if(rows < 1) return response(message, ":x: Nie znaleziono Cię w bazie danych. Jeżeli jesteś nowym członkiem serwera, zostaniesz wkrótce dodany do bazy danych. Jeśli jesteś już jakiś czas na serwerze i wysłałeś na nim conajmniej kilka wiadomości to skontaktuj się z zarządem serwera (najlepiej z osobą która ma rolę \"Server Technician\"; może być to spowodowane błędem w bazie danych).");
+            if(rows.length < 1) return response(message, `\`STAN AKTYWNOŚCI DLA ${member.displayName}\` \n Punkty doświadczenia: **0** / **50** (**0%**) \n Suma całego zdobytego XP: **0** \n Poziom: **1**`);
         
-            database.query(`SELECT * FROM members ORDER BY totalXp DESC`, (err1, rows1) => {
+            database.query(`SELECT * FROM members WHERE totalXp LIKE "%${message.guild.id}%"`, (err1, rows1) => {
                 if(err1) {
                     console.log(err1);
                     return response(message, ":x: Wystąpił błąd podczas łączenia się z bazą danych (2). Spróbuj ponownie później.");
                 }
 
-                var rank = rows1.length + 1;
+                var xpArray = new Array(rows1.length);
+                var levelArray = new Array(rows1.length);
+                var totalXpArray = new Array(rows1.length);
                 for(var i = 0; i < rows1.length; i++) {
-                    if(rows1[i].discordID === `${member.id}`) rank = i + 1;
+                    let xpServer = decode1(rows1[i].xp);
+                    let levelServer = decode1(rows1[i].level);
+                    let totalXpServer = decode1(rows1[i].totalXp);
+                    for(var j = 0; j < totalXpServer.length; j++) {
+                        if(totalXpServer[j].startsWith(message.guild.id)) {
+                            xpArray[i] = member.id + ":" + decode2(xpServer[i])[1];
+                            levelArray[i] = member.id + ":" + decode2(levelServer[i])[1];
+                            totalXpArray[i] = member.id + ":" + decode2(totalXpServer[i])[1];
+                        }
+                    }
                 }
 
-                var nextLevel = rows[0].level * 50;
-                response(message, `\`STAN AKTYWNOŚCI DLA ${member.displayName}\` \n Punkty doświadczenia: **${rows[0].xp}** / **${nextLevel}** (**${Math.floor(rows[0].xp / nextLevel * 100)}%**) \n Suma wszystkich zdobytych PD: **${rows[0].totalXp}** \n Poziom: **${rows[0].level}** \n Miejsce w rankingu: **#${rank}**`);
+                totalXpArray.sort();
+
+                var rank = totalXpArray.length + 1;
+                var thisXp;
+                var thisLevel;
+                var thisTotalXp;
+                for(var i = 0; i < totalXpArray.length; i++) {
+                    var id = decode2(totalXpArray[i])[0];
+                    var tx = decode2(totalXpArray[i])[1];
+                    if(id === member.id) {
+                        rank = i + 1;
+                        thisTotalXp = tx;
+
+                        for(var j = 0; j < totalXpArray.length; j++) {
+                            if(decode2(xpArray[j])[0] === member.id) {
+                                thisXp = decode2(xpArray[j])[1];
+                                thisLevel = decode2(levelArray[j])[1];
+                            }
+                        }
+                    }
+                }
+
+                var nextLevel = thisLevel * 50;
+                response(message, `\`STAN AKTYWNOŚCI DLA ${member.displayName}\` \n Punkty doświadczenia: **${thisXp}** / **${nextLevel}** (**${Math.floor(thisXp / nextLevel * 100)}%**) \n Suma wszystkich zdobytych PD: **${thisTotalXp}** \n Poziom: **${thisLevel}** \n Miejsce w rankingu: **#${rank}**`);
             });
         });
     });
@@ -54,4 +91,22 @@ module.exports.config = {
 
 function response(message, response) {
     message.channel.send(response);
+}
+
+function decode1(text) {
+    let fields = text.split("/NF");
+
+    return fields;
+}
+
+function decode2(field) {
+    let parts = field.split(":");
+
+    return parts;
+}
+
+function decode3(part) {
+    let details = part.split(",");
+
+    return details;
 }

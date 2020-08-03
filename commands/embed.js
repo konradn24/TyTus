f69f58eddda8bfdd5a647b57cb7aa74f2a0888f2
konradn24 <54041190+konradn24@users.noreply.github.com>
2020-08-03 12:03:52 +0200

    Update embed.js

--- commands/embed.js
+++ commands/embed.js
@@ -15,7 +15,7 @@ module.exports.run = async (bot, message, args, database) => {
     .setColor(colors.gold)
     .setDescription(text);
 
-    var messageID = await channel.send(embed);
+    var messageID = await channel.send(embed).id;
     var channelID = channel.id;
     var guildID = message.guild.id;
     await message.channel.send(`:white_check_mark: Gotowe! Kliknij w link, żeby zobaczyć wiadomość: https://discordapp.com/channels/${guildID}/${channelID}/${messageID}`);
